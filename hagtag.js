291e53ee876f984ae314d95894175d01a62e578f
Gabe <finkegabriel>
2018-12-20 23:58:38 -0700

    end

--- hagtag.js
+++ hagtag.js
@@ -1,11 +1,28 @@
 var twitterAPI = require('node-twitter-api');
 var keys = require('./secret/keys');
 
-
 var twitter = new twitterAPI({
     consumerKey: keys.apiKey,
     consumerSecret: keys.apiKeySecret,
     callback: keys.callbackUrl,
+});
+
+twitter.getRequestToken(function(error, requestToken, requestTokenSecret, results){
+    if (error) {
+        console.log("Error getting OAuth request token : " + error);
+    } else {
+        console.log("token request "+requestToken);
+        console.log("token secret ")+requestTokenSecret;
+    }
+});
+
+twitter.getAccessToken(requestToken, requestTokenSecret, oauth_verifier, function(error, accessToken, accessTokenSecret, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        //store accessToken and accessTokenSecret somewhere (associated to the user)
+        //Step 4: Verify Credentials belongs here
+    }
 });
 
 module.exports = {
