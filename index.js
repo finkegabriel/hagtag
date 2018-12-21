b6985da5a51d1f4ed33a8f185e820c526046f519
Gabe <finkegabriel>
2018-12-20 19:40:59 -0700

    done for the night

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 var app = express();
+const path = require('path');
+const publicPath = path.join(__dirname, './hbs');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use('/', express.static(publicPath));
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    res.send('Hello World');
- });
+   res.render('index');
+});
 
  var server = app.listen(2018, function () {
     var host = server.address().address
